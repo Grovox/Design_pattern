238c81c35a966bfd526e6444962acd0f6f500eea
Grovox <Grovox@bk.ru>
2023-02-17 11:27:38 +0300

    Facade

---
+++ src/structure/facade/Job.java
@@ -0,0 +1,7 @@
+package structure.facade;
+
+public class Job {
+    public void doJob(){
+        System.out.println("Job in progress...");
+    }
+}
---
+++ src/structure/facade/SprintRunner.java
@@ -0,0 +1,9 @@
+package structure.facade;
+
+public class SprintRunner {
+    public static void main(String[] args) {
+        WorkFlow workFlow = new WorkFlow();
+
+        workFlow.solveProblems();
+    }
+}
