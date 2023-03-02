3c34b6c3cca2f1e3550c97ad076c8f480c7b22aa
Grovox <Grovox@bk.ru>
2023-03-02 16:52:20 +0300

    Memento

---
+++ src/behavioral/memento/Save.java
@@ -0,0 +1,24 @@
+package behavioral.memento;
+
+import behavioral.command.Database;
+
+
+import java.util.Date;
+
+public class Save {
+    private final String version;
+    private final Date data;
+
+    public Save(String version) {
+        this.version = version;
+        this.data = new Date();
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Date getData() {
+        return data;
+    }
+}
